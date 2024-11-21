5937961bcfe5df6f8f7993b7fb4e0e5ffc538a23
vinny-pereira <vinicius@web4you.ca>
2024-11-21 18:51:18 -0500

    fixed submission 004 script for correct address derivation based on 100 index

--- submission/004.sh
+++ submission/004.sh
@@ -1,4 +1,4 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
-checksum=$(bitcoin-cli getdescriptorinfo "tr($xpub/0/*)" | jq -r .checksum)
-bitcoin-cli deriveaddresses "tr($xpub/0/*)#$checksum" "[100,100]"
+descriptor=$(bitcoin-cli getdescriptorinfo "tr($xpub/100)" | jq -r .descriptor)
+bitcoin-cli deriveaddresses "$descriptor" | jq .[0]
