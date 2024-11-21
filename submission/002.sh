16dccfeb033497fd61504473bbad8509d32bc0fc
vinny-pereira <vinicius@web4you.ca>
2024-11-20 21:30:22 -0500

    answered single line questions

--- submission/002.sh
+++ submission/002.sh
@@ -1,4 +1,6 @@
 # (true / false) Verify the signature by this address over this message:
-#   address: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
-#   message: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
-#   signature: `HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM=`
+address="1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
+message="1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
+signature="HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM="
+
+bitcoin-cli verifymessage $address $signature $message
