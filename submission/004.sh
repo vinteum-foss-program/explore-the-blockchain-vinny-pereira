eb77b4187b8d5d07853d1e5d2d82e4cbeb1085f0
vinny-pereira <vinicius@web4you.ca>
2024-11-21 18:54:33 -0500

    fixed submission 004 output format

--- submission/004.sh
+++ submission/004.sh
@@ -1,4 +1,4 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 descriptor=$(bitcoin-cli getdescriptorinfo "tr($xpub/100)" | jq -r .descriptor)
-bitcoin-cli deriveaddresses "$descriptor" | jq .[0]
+bitcoin-cli deriveaddresses "$descriptor" | jq -r .[0]
