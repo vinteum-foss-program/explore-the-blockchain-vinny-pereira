3c53a3f33ca15f0a59486c24f6a3f50a2366f63f
vinny-pereira <vinicius@web4you.ca>
2024-11-23 12:53:29 -0500

    submission 006

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,12 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+coinbasetx=$(bitcoin-cli getblock $(bitcoin-cli getblockhash 256128) | jq -r .tx[0])
+blocktxs=$(bitcoin-cli getblock $(bitcoin-cli getblockhash 257343) | jq -r .tx[])
+
+for tx in $blocktxs
+do
+    match=$(bitcoin-cli getrawtransaction $tx true | jq --arg coinbasetx "$coinbasetx" -r '.vin[] | select(has("txid") and .txid == $coinbasetx) | true')
+    if [ $match ]; then
+        echo $tx
+        break
+    fi
+done
